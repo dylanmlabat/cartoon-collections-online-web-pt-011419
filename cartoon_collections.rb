b9dd2f4be2e6fe6fe594902f26869b05dc66c29a
Dylan Michael Labat <dml252@live.com>
2019-01-21 16:33:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,8 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  # Your code here
+  array.any? do |call|
+    call.length > 4
 end
 
 def find_the_cheese# code an argument here
