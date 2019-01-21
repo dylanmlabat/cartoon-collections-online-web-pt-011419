7ca931c18519bf1901d9cade54ce0a328f4baa9a
Dylan Michael Labat <dml252@live.com>
2019-01-21 16:30:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,10 @@
-def roll_call_dwarves(array) # code an argument here
+def roll_call_dwarves(array)
   array.each_with_index do |name, number|
     puts "#{number + 1}. #{name}"
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
   # Your code here
 end
 
