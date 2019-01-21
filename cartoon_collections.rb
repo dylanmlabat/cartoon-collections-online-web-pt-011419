54cc5d02751ae94111f401ef516435264251b97b
Dylan Michael Labat <dml252@live.com>
2019-01-21 16:38:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |food|
-    food.find?[cheese_types]
+    cheese_types.include?[food]
   end
 end
